306751852bc19477d841d957f51cfd6f44e5ba48
Jeff Ching <jching@avvo.com>
2013-09-10 14:43:20 -0700

    fix default configuration for railtie

--- lib/cache_debugging/railtie.rb
+++ lib/cache_debugging/railtie.rb
@@ -1,12 +1,12 @@
 module CacheDebugging
   class Railtie < Rails::Railtie
-    config.cache_debugging = ActiveSupport::OrderedOptions.new(
-      # raise exceptions if templates aren't in the dependency tree
-      strict_dependencies: false,
+    config.cache_debugging = ActiveSupport::OrderedOptions.new
 
-      # [0,1] - decimal (percent) of cache hits to check
-      view_sampling: 0
-    )
+    # raise exceptions if templates aren't in the dependency tree
+    config.cache_debugging.strict_dependencies = false
+
+    # [0,1] - decimal (percent) of cache hits to check
+    config.cache_debugging.view_sampling = 0
 
     initializer "cache_debugging.setup", :before => 'cache_digests' do |app|
       ActiveSupport.on_load(:action_view) do
