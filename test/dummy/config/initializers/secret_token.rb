bb00e0961982ba7d52e82845dc4d3aeb2fb8ff76
Jeff Ching <jching@avvo.com>
2013-09-10 13:12:49 -0700

    tests should run on rails 3.2

--- test/dummy/config/initializers/secret_token.rb
+++ test/dummy/config/initializers/secret_token.rb
@@ -9,4 +9,8 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Dummy::Application.config.secret_key_base = '4adc573725714bbd95224181f48aa3cc22055e9995c73e3a2a6f2e899281c92423ad082543254d79a711298416a8cc7d3d486d5b7ad5874c72dfcc3ac9645d7f'
+if Rails::VERSION::MAJOR == 4
+  Dummy::Application.config.secret_key_base = '4adc573725714bbd95224181f48aa3cc22055e9995c73e3a2a6f2e899281c92423ad082543254d79a711298416a8cc7d3d486d5b7ad5874c72dfcc3ac9645d7f'
+else
+  Dummy::Application.config.secret_token = '4adc573725714bbd95224181f48aa3cc22055e9995c73e3a2a6f2e899281c92423ad082543254d79a711298416a8cc7d3d486d5b7ad5874c72dfcc3ac9645d7f'
+end
